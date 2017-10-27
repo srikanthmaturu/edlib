8cd3215c97699fceeb8198141abc2a15038a4b1b
Srikanth Maturu <srikanthmaturu@users.noreply.github.com>
2017-10-27 10:13:19 -0500

    fixes

---
+++ apps/aligner/aligner2.cpp
@@ -0,0 +1,198 @@
+#include <unistd.h>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+#include <vector>
+#include <ctime>
+#include <string>
+#include <climits>
+#include <queue>
+#include <fstream>
+#include <iostream>
+#include <edlib.h>
+
+#include "edlib.h"
+
+using namespace std;
+
+int readFastaSequences(const char* path, vector< vector<char> >* seqs);
+
+void printAlignment(const char* query, const char* target,
+                    const unsigned char* alignment, const int alignmentLength,
+                    const int position, const EdlibAlignMode modeCode);
+
+// For debugging
+void printSeq(const vector<char> &seq) {
+    for (int i = 0; i < (int) seq.size(); i++)
+        printf("%d ", seq[i]);
+    printf("\n");
+}
+
+string alignSequences(vector<char> firstSequence, vector<char> secondSequence, EdlibAlignConfig edlibAlignConfig){
+    //cout << string{firstSequence.begin(), firstSequence.end()} << " \n" << string{secondSequence.begin(), secondSequence.end()} << endl;
+    EdlibAlignResult alignResult = edlibAlign(firstSequence.data(), firstSequence.size(), secondSequence.data(), secondSequence.size(), edlibAlignConfig);
+    int tIdx = alignResult.endLocations[0], qIdx = -1;
+    for (int i = 0; i < alignResult.alignmentLength; i++) {
+        if (alignResult.alignment[i] != EDLIB_EDOP_INSERT)
+            tIdx--;
+    }
+    //cout << alignResult.startLocations[0] << "\t" << tIdx << "\t" <<endl;
+    string alignSequence;
+    if(tIdx >= 0){
+        alignSequence = {secondSequence.begin(), secondSequence.begin() + tIdx + 1};
+    }
+    auto alignment = alignResult.alignment;
+    for (int i = 0; i < alignResult.alignmentLength; i++) {
+        switch(alignment[i]){
+            case EDLIB_EDOP_DELETE:
+                alignSequence = alignSequence + secondSequence[++tIdx];
+                //cout << "DELETE" << "\t";
+                break;
+            case EDLIB_EDOP_INSERT:
+                alignSequence = alignSequence + firstSequence[++qIdx];
+                //cout << "INSERT" << "\t";
+                break;
+            case EDLIB_EDOP_MATCH:
+                alignSequence = alignSequence + secondSequence[++tIdx];
+                ++qIdx;
+                //cout << "MATCH" << "\t";
+                break;
+            case EDLIB_EDOP_MISMATCH:
+                alignSequence = alignSequence + secondSequence[++tIdx];
+                ++qIdx;
+                //cout << "MISMATCH" << "\t";
+        }
+    }
+    cout << endl;
+    if(tIdx < secondSequence.size()){
+        alignSequence = alignSequence + string{secondSequence.begin() + tIdx + 1, secondSequence.end()};
+    }
+    printAlignment(firstSequence.data(), secondSequence.data(),  alignResult.alignment, alignResult.alignmentLength,  *(alignResult.endLocations), EDLIB_MODE_HW);
+    edlibFreeAlignResult(alignResult);
+    return alignSequence;
+}
+
+int main(int argc, char* const argv[]){
+    if(argc < 3){
+        cout << "Usage ./aligner2 [sequences_file] [output_file]" << endl;
+        return 1;
+    }
+    EdlibAlignConfig edlibConfig = edlibNewAlignConfig(-1, EDLIB_MODE_HW, EDLIB_TASK_PATH, NULL, 0);
+
+    int readResult;
+    // Read queries
+    char* sequenceFilepath = argv[1];
+    vector< vector<char> >* sequences = new vector< vector<char> >();
+    printf("Reading sequences... \n");
+    readResult = readFastaSequences(sequenceFilepath, sequences);
+    cout << "Number of sequences: " << (sequences)->size() << endl;
+    if (readResult) {
+        printf("Error: There is no file with name %s\n", sequenceFilepath);
+        delete sequences;
+        return 1;
+    }
+    ofstream results(argv[2], ofstream::out);
+
+    for(uint64_t i = 0; i < sequences->size(); i++){
+        for(uint64_t j = i + 1; j < sequences->size(); j++){
+            results << ">Alignment of sequences " << i << " and " << j << endl;
+            cout << ">Alignment of sequences " << i << " and " << j << endl;
+            string alignedSequence = alignSequences((*sequences)[i], (*sequences)[j], edlibConfig);
+            results << alignedSequence << endl;
+        }
+    }
+}
+
+/** Reads sequences from fasta file.
+ * @param [in] path Path to fasta file containing sequences.
+ * @param [out] seqs Sequences will be stored here, each sequence as vector of letters.
+ * @return 0 if all ok, positive number otherwise.
+ */
+int readFastaSequences(const char* path, vector< vector<char> >* seqs) {
+    seqs->clear();
+
+    FILE* file = fopen(path, "r");
+    if (file == 0)
+        return 1;
+
+    bool inHeader = false;
+    bool inSequence = false;
+    const int buffSize = 4096;
+    char buffer[buffSize];
+    while (!feof(file)) {
+        int read = fread(buffer, sizeof(char), buffSize, file);
+        for (int i = 0; i < read; ++i) {
+            char c = buffer[i];
+            if (inHeader) { // I do nothing if in header
+                if (c == '\n')
+                    inHeader = false;
+            } else {
+                if (c == '>') {
+                    inHeader = true;
+                    inSequence = false;
+                } else {
+                    if (c == '\r' || c == '\n')
+                        continue;
+                    // If starting new sequence, initialize it.
+                    if (inSequence == false) {
+                        inSequence = true;
+                        seqs->push_back(vector<char>());
+                    }
+                    seqs->back().push_back(c);
+                }
+            }
+        }
+    }
+
+    fclose(file);
+    return 0;
+}
+
+
+void printAlignment(const char* query, const char* target,
+                    const unsigned char* alignment, const int alignmentLength,
+                    const int position, const EdlibAlignMode modeCode) {
+    int tIdx = -1;
+    int qIdx = -1;
+    if (modeCode == EDLIB_MODE_HW) {
+        tIdx = position;
+        for (int i = 0; i < alignmentLength; i++) {
+            if (alignment[i] != EDLIB_EDOP_INSERT)
+                tIdx--;
+        }
+    }
+    for (int start = 0; start < alignmentLength; start += 50) {
+        // target
+        printf("T: ");
+        int startTIdx;
+        for (int j = start; j < start + 50 && j < alignmentLength; j++) {
+            if (alignment[j] == EDLIB_EDOP_INSERT)
+                printf("-");
+            else
+                printf("%c", target[++tIdx]);
+            if (j == start)
+                startTIdx = tIdx;
+        }
+        printf(" (%d - %d)\n", max(startTIdx, 0), tIdx);
+
+        // match / mismatch
+        printf("   ");
+        for (int j = start; j < start + 50 && j < alignmentLength; j++) {
+            printf(alignment[j] == EDLIB_EDOP_MATCH ? "|" : " ");
+        }
+        printf("\n");
+
+        // query
+        printf("Q: ");
+        int startQIdx = qIdx;
+        for (int j = start; j < start + 50 && j < alignmentLength; j++) {
+            if (alignment[j] == EDLIB_EDOP_DELETE)
+                printf("-");
+            else
+                printf("%c", query[++qIdx]);
+            if (j == start)
+                startQIdx = qIdx;
+        }
+        printf(" (%d - %d)\n\n", max(startQIdx, 0), qIdx);
+    }
+}
